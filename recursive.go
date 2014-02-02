45968034352400078f86a14b9730e200ea539f25
Pavel Polyakov <me@pavelpolyakov.com>
2014-02-02 22:18:41 +0200

    sync

--- recursive.go
+++ recursive.go
@@ -6,10 +6,10 @@ func recursiveCopy(p map[string]interface{}, key string,  m interface{}) {
 	switch m.(type) {
 	case map[string]interface{}:
 		p[key] = make(map[string]interface{})
-		newP := p[key]
+		//newP := p[key]
 		//p = p[key]
 		for k,v := range m.(map[string]interface{}) {
-			recursiveCopy(newP.(map[string]interface{}), k, v)
+			recursiveCopy(p[key].(map[string]interface{}), k, v)
 		}
 	default:
 		p[key] = m
@@ -22,10 +22,15 @@ func main() {
 	hello["first"].(map[string]interface{})["second"] = make(map[string]interface{})
 	hello["first"].(map[string]interface{})["second"].(map[string]interface{})["last"] = 123
 
+	helloAtom := make(map[string]interface{})
+	helloAtom["first"] = 333
+
 	var helloPointer map[string]interface{} = make(map[string]interface{})
+	var helloAtomPointer map[string]interface{} = make(map[string]interface{})
 	//helloPointer = make(map[string]interface{})
 
 	recursiveCopy(helloPointer, "first", hello["first"])
+	recursiveCopy(helloAtomPointer, "first", helloAtom["first"])
 
 	//helloPointer := hello
 
@@ -33,7 +38,16 @@ func main() {
 
 	fmt.Println(hello)
 	fmt.Println("---")
-	//helloPointer["first"].(map[string]interface{})["second"].(map[string]interface{})["last"] = 124
 	fmt.Println(helloPointer)
+	fmt.Println("---")
+
+	hello["first"].(map[string]interface{})["second"].(map[string]interface{})["last"] = 125
+
+	fmt.Println(hello)
+	fmt.Println("---")
+	fmt.Println(helloPointer)
+
+	fmt.Println("---")
+	fmt.Println(helloAtomPointer)
 
 }
