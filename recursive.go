356fc5cd734b5e47974629ffd1383978855b0c0b
Pavel Polyakov <me@pavelpolyakov.com>
2014-02-03 00:07:06 +0200

    sync

--- recursive.go
+++ recursive.go
@@ -1,13 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"reflect"
+	//"os"
+)
 
 func recursiveCopy(p map[string]interface{}, key string,  m interface{}) {
 	switch m.(type) {
 	case map[string]interface{}:
 		p[key] = make(map[string]interface{})
-		//newP := p[key]
-		//p = p[key]
 		for k,v := range m.(map[string]interface{}) {
 			recursiveCopy(p[key].(map[string]interface{}), k, v)
 		}
@@ -16,7 +18,47 @@ func recursiveCopy(p map[string]interface{}, key string,  m interface{}) {
 	}
 }
 
+type Record struct {
+	Value Value
+}
+
+type Value interface{}
+
+func (r *Record) Fill(key string, rParent Record) {
+	switch rParent.Value.(type) {
+	case map[string]Record:
+		fmt.Println("hello")
+		//os.Exit(1)
+		r.Value.(map[string]Record)[key] = Record{Value:make(map[string]Record)}
+	for k,v := range rParent.Value.(map[string]Record) {
+		nextR := r.Value.(map[string]Record)[key]
+		nextR.Fill(k, v)
+	}
+	default:
+		r.Value.(map[string]Record)[key] = Record{Value:rParent.Value}
+	}
+}
+
 func main() {
+
+	r := Record{Value:make(map[string]Record)}
+
+	r.Value.(map[string]Record)["first"] = Record{Value:make(map[string]Record)}
+	r.Value.(map[string]Record)["first"].Value.(map[string]Record)["second"] = Record{Value:make(map[string]interface{})}
+	r.Value.(map[string]Record)["first"].Value.(map[string]Record)["second"].Value.(map[string]interface{})["third"] = "hello"
+
+	fmt.Println(r)
+
+	rChild := Record{Value:make(map[string]Record)}
+
+	rChild.Fill("first", r.Value.(map[string]Record)["first"])
+
+	fmt.Println(rChild)
+//	fmt.Println(rChild.Value.(map[string]Record)["first"].Value.(map[string]Record)["second"])
+	return
+
+
+
 	hello := make(map[string]interface{})
 	hello["first"] = make(map[string]interface{})
 	hello["first"].(map[string]interface{})["second"] = make(map[string]interface{})
